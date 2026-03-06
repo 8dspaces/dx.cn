8c18f7b8b80c0a93663cc061c9191ac1ef7b63d2
8dspaces <mickqi1985@outlook.com>
2026-03-06 19:30:01 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/dx.cn' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dx.cn' : '',
+  
+  basePath: '',
+  assetPrefix: '',
+  
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  
   trailingSlash: true,
 }
 
